0153680fb4facfb3aa40b7cad0a4d0e68fb06d20
Kai Hotz <kaihotz@me.com>
2018-05-12 18:01:24 +0200

    - reducer adjustment

--- app/src/reducers/reducer_listings.js
+++ app/src/reducers/reducer_listings.js
@@ -1,6 +1,6 @@
 import { FETCH_LISTINGS } from '../actions'
 
-export default function (state = [], action) {
+export default (state = [], action) => {
   switch (action.type) {
     case FETCH_LISTINGS:
       return action.payload

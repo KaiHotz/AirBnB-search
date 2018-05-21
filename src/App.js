65278150048bfb4107e185d25cfe9b5694d0b479
Kai Hotz <kaihotz@me.com>
2018-05-21 20:05:57 +0200

    - Turned App.js into functional component since it is only used to represent the layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { compose } from 'recompose'
 import WithErrors from './hocs/WithErrors'
 import Search from './components/Search'
 import Listings from './containers/Listings'
-
 import './styles/styles.scss'
 
-class App extends Component {
-  render () {
-    return (
-      <Fragment>
-        <Search />
-        <Listings />
-      </Fragment>
-    )
-  }
-}
+const App = () => (
+  <Fragment>
+    <Search />
+    <Listings />
+  </Fragment>
+)
 
 export default compose(
   WithErrors

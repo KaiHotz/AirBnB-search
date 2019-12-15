5852786cbb613558b0abf846b0c1d14ed562b703
Kai Hotz <kaihotz@me.com>
2019-12-15 16:28:23 +0100

    Updates

---
+++ precache-manifest.6a12fdaa4c89f5d760232e4293b2f9bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d1ecf8848d90b91fe766d712f5a1cd",
+    "url": "/AirBnB-search/index.html"
+  },
+  {
+    "revision": "4298c9df3ba0b56ba661",
+    "url": "/AirBnB-search/static/css/2.21d3525d.chunk.css"
+  },
+  {
+    "revision": "9f65f875e35da3573d9d",
+    "url": "/AirBnB-search/static/css/main.e64e1587.chunk.css"
+  },
+  {
+    "revision": "4298c9df3ba0b56ba661",
+    "url": "/AirBnB-search/static/js/2.7c867285.chunk.js"
+  },
+  {
+    "revision": "03a03e2a3a7706ed5c5cdf9ef2516048",
+    "url": "/AirBnB-search/static/js/2.7c867285.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f65f875e35da3573d9d",
+    "url": "/AirBnB-search/static/js/main.7c716a6c.chunk.js"
+  },
+  {
+    "revision": "0627f084a8eaeaf576a6",
+    "url": "/AirBnB-search/static/js/runtime~main.2d6cf07c.js"
+  },
+  {
+    "revision": "ca7ae69605369eb452b5bd18c46c8e54",
+    "url": "/AirBnB-search/static/media/loading_animation_circle.ca7ae696.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AirBnB-search/precache-manifest.d5ba6c89d93caaa631f4b02afcdd9862.js"
+  "/AirBnB-search/precache-manifest.6a12fdaa4c89f5d760232e4293b2f9bb.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AirBnB-search/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AirBnB-search/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
